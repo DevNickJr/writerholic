7359f66fb7f693c66b37e591b6e9fd7a097f9624
DevNickjr <nicholasduadei14@gmail.com>
2024-10-11 08:56:39 +0100

    setup mongodb and auth session

---
+++ src/constants/siteMetaData.ts
@@ -0,0 +1,21 @@
+// please update required information
+export const siteMetadata = {
+    title: 'Next.js Blog With Tailwind CSS and Contentlayer',
+    author: 'writeholic',
+    headerTitle: 'Next.js Blog',
+    description: 'A blog created with Next.js, Tailwind.css and contentlayer.',
+    language: 'en-us',
+    theme: 'system', // system, dark or light
+    siteUrl: 'https://create-blog-with-nextjs.vercel.app', // your website URL
+    siteLogo: '/logo.png',
+    socialBanner: '/social-banner.png', // add social banner in the public folder
+    email: 'writeholic27@gmail.com', 
+    github: 'https://github.com/writeholic27',
+    twitter: 'https://twitter.com/code_bucks',
+    facebook: 'https://facebook.com',
+    youtube: 'https://youtube.com/writeholic',
+    linkedin: 'https://www.linkedin.com/in/writeholic/',
+    dribbble: 'https://www.dribbble.com',
+    locale: 'en-US',
+}
+  
